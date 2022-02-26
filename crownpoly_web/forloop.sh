12e9b2ebdabe21228536aa01e7c080cd68fc3bb8
Ayomide <ayo.com>
2022-02-25 23:18:45 -0700

    both forloop and softwares updated

--- crownpoly_web/forloop.sh
+++ crownpoly_web/forloop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #ForLoop for 100 to 90 in ascending order.
-echo  "ForLoop starts."
+echo  "ForLoop starts here."
 for ((a=90;a<=100;a++))
 do
 echo "$a"
 done
-echo "ForLoop ends."
+echo "ForLoop ends here."
